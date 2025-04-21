6afdece66d21272b743fa2cd744f738821dbc328
Yusuf Çağrı Yavuz <44903095+HeribertYavuz@users.noreply.github.com>
2025-04-21 22:14:51 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_p13blue2022kk1-user
-add_lunch_combo twrp_p13blue2022kk1-userdebug
-add_lunch_combo twrp_p13blue2022kk1-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_p13blue2022kk1-user \
+    twrp_p13blue2022kk1-userdebug \
+    twrp_p13blue2022kk1-eng
