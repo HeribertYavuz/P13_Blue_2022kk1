8e90ef060a2a5f252bbb825a334b81cecc58205e
Yusuf Çağrı Yavuz <44903095+HeribertYavuz@users.noreply.github.com>
2025-04-21 22:06:57 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_P13_Blue_2022kk1.mk
+    $(LOCAL_DIR)/twrp_p13blue2022kk1.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_P13_Blue_2022kk1-user \
-    twrp_P13_Blue_2022kk1-userdebug \
-    twrp_P13_Blue_2022kk1-eng
+    twrp_p13blue2022kk1-user \
+    twrp_p13blue2022kk1-userdebug \
+    twrp_p13blue2022kk1-eng
